aec3c48b2baf8f671b62fdb3fcbce162ffa0d59d
Fred K. Schott <fkschott@gmail.com>
2019-06-14 15:48:12 -0700

    update warning message

--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -40,14 +40,14 @@ export async function run(config: Config, reporter: Reporter, flags: Flags, args
   const contentsArg = flags.contents ? [] : ['--contents', flags.out || flags.contents || 'pkg/'];
   const manifest = await config.loadPackageManifest();
   if (!manifest.scripts.version) {
-    reporter.warn(`${chalk.bold('"version" script missing!')} A fresh build is required after every bump to the master package.json version.`);
+    reporter.warn(`${chalk.bold('missing "version" script:')} You'll want to create a fresh build after bumping the master package.json version.`);
     if (manifest.scripts.build) {
       config._manifest.scripts.version = 'npm run build';
     } else {
       config._manifest.scripts.version = 'npx @pika/pack build';
     }
-    await config.savePackageManifest(config._manifest);
     reporter.log(`Adding the following "version" lifecycle script to your package.json... ` + chalk.bold(`"${config._manifest.scripts.version}"`));
+    await config.savePackageManifest(config._manifest);
     reporter.log(`Please review & commit this change before publishing.`);
     return;
   }
